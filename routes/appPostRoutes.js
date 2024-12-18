f83a6a89b030d3dbe515365f0514188390589fbf
Igor Mikhaylov <igormikhaylov@MacBook-Pro-Igor.local>
2024-12-18 19:45:37 +0300

    18.12.5

--- routes/appPostRoutes.js
+++ routes/appPostRoutes.js
@@ -1,5 +1,86 @@
 const User = require("../models/User");
-const projects = require("./tasks.js")
+const projects = [
+  {
+    name: "Epic Battle Quest",
+    buttonText: "Скачать",
+    description: "Скачай и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "all",
+    shortName: "epic_battle_quest",
+  },
+  {
+    name: "Mystic Puzzle Saga",
+    buttonText: "Зарегистрироваться",
+    description: "Зарегистрируйся и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "android",
+    shortName: "mystic_puzzle_saga",
+  },
+  {
+    name: "Galaxy Racer X",
+    buttonText: "Скачать",
+    description: "Скачай и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "ios",
+    shortName: "galaxy_racer_x",
+  },
+  {
+    name: "Fantasy Kingdom",
+    buttonText: "Зарегистрироваться",
+    description: "Зарегистрируйся и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "all",
+    shortName: "fantasy_kingdom",
+  },
+  {
+    name: "Adventure Runner",
+    buttonText: "Скачать",
+    description: "Скачай и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "android",
+    shortName: "adventure_runner",
+  },
+  {
+    name: "Zombie Survival 3D",
+    buttonText: "Зарегистрироваться",
+    description: "Зарегистрируйся и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "ios",
+    shortName: "zombie_survival_3d",
+  },
+  {
+    name: "Space Miner",
+    buttonText: "Скачать",
+    description: "Скачай и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "all",
+    shortName: "space_miner",
+  },
+  {
+    name: "Treasure Hunt",
+    buttonText: "Зарегистрироваться",
+    description: "Зарегистрируйся и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "android",
+    shortName: "treasure_hunt",
+  },
+  {
+    name: "Warrior's Journey",
+    buttonText: "Скачать",
+    description: "Скачай и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "ios",
+    shortName: "warriors_journey",
+  },
+  {
+    name: "Magic Duel",
+    buttonText: "Зарегистрироваться",
+    description: "Зарегистрируйся и получи +1 прокрут",
+    link: "https://www.google.ru/?hl=ru",
+    platform: "all",
+    shortName: "magic_duel",
+  },
+];
 
 const getRandomPrize = () => {
   const round = ["iphone", "0", "10.000", "30.000", "0", "500", "40.000", "0", "500", "10.000", "0", "500"];
