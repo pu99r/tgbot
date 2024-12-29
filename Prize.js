716f810b03231b33d9041867a6f7aa84e05eaf86
Igor Mikhaylov <igormikhaylov@MBP-Igor.lan>
2024-12-29 03:11:12 +0300

    29.12.16

--- Prize.js
+++ Prize.js
@@ -1,9 +1,8 @@
-// getRandomPrize.js
 const fs = require("fs").promises;
 const path = require("path");
 const User = require("./models/User");
 
-const getRandomPrize = async (telegramId) => {
+const getRandomPrize = async (telegramId, spins) => {
   const round = [
     "iphone",
     "0",
@@ -18,27 +17,36 @@ const getRandomPrize = async (telegramId) => {
     "0",
     "500",
   ];
-  const prizes = ["iphone", "40.000", "30.000", "10.000", "500", "0"];
-//   const prizes = ["500"]; // <-- здесь выбирается, что именно может выпасть
-  const priz = prizes[Math.floor(Math.random() * prizes.length)];
 
+  const filePath = path.join(__dirname, "codes.txt");
+  let isCodesFileNotEmpty = false;
+
+  try {
+    const data = await fs.readFile(filePath, "utf8");
+    const lines = data.split("\n").filter((line) => line.trim() !== "");
+    isCodesFileNotEmpty = lines.length > 0;
+  } catch (err) {
+    console.error("Ошибка при проверке файла с кодами:", err);
+  }
+
+  const prizes = ["iphone", "10.000", "0"];
+  if (isCodesFileNotEmpty) {
+    prizes.push("500");
+  }
+
+  const priz = prizes[Math.floor(Math.random() * prizes.length)];
   const indices = round
     .map((value, index) => (value === priz ? index : -1))
     .filter((index) => index !== -1);
   const indexof = indices[Math.floor(Math.random() * indices.length)];
 
-  // Если приз "500", то забираем код из файла codes.txt
   if (priz === "500") {
     try {
-      const filePath = path.join(__dirname, "codes.txt");
       let data = await fs.readFile(filePath, "utf8");
       let lines = data.split("\n").filter((line) => line.trim() !== "");
       if (lines.length > 0) {
-        const codeFromFile = lines.shift(); // берём первый код
-        // перезаписываем файл, вырезав уже используемый код
+        const codeFromFile = lines.shift();
         await fs.writeFile(filePath, lines.join("\n"));
-
-        // Сохраняем код пользователю
         const user = await User.findOne({ telegramId });
         if (user) {
           user.codes.push(codeFromFile.trim());
