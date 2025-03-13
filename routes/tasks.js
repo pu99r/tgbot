9a68e3bfc60fea29fb5222ffaa0baedef9c0127c
Igor Mikhaylov <igormikhaylov@MacBook-Pro-Igor.local>
2025-03-13 20:48:49 +0300

    13.03.02.11

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,3 +1,4 @@
+// https://api.telegram.org/bot7688745445:AAE4cOnNWghqnlNN29I4oQy-XT8fa3Mi0jc/getChatMember?chat_id=1002265070330&user_id=1370034279
 const projects = [
   {
     name: "Wb Купоны",
@@ -22,7 +23,7 @@ const projects = [
     buttonText: "Подписатья",
     description: "Подпишись и получи 3 звезды",
     id: "-1002517539275",
-    link: "https://t.me/+EcbCfB6PVxgyYzcy",
+    link: "https://t.me/+h6CDRW08l2o0MTJi",
     prize: "balance3",
     shortName: "Test1",
   },
@@ -31,7 +32,7 @@ const projects = [
     buttonText: "Подписатья",
     description: "Подпишись и получи 3 звезды",
     id: "-1002518938854",
-    link: "https://t.me/+EcbCfB6PVxgyYzcy",
+    link: "https://t.me/+pLGt50dCu00wZDIy",
     prize: "balance3",
     shortName: "Test2",
   },
